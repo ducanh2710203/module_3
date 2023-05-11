adc64718fd8e645b3063c71b9465c41adab282e3
ducanh2710203 <duc2710203@gmail.com>
2023-05-11 10:51:17 +0700

    cm

---
+++ 2. Xử lý bất đồng bộ 1/Callback.js
@@ -0,0 +1,14 @@
+function download(url,callback) {
+    setTimeout(() => {
+        console.log(`Downloading ${url} ...`);
+        callback(url)
+    }, 1000)
+
+
+}
+function process(picture) {
+    console.log(`Processing ${picture}`);
+}
+let url = 'https://www.javascripttutorial.net/pic.jpg';
+download(url,process);
+// process(url);
